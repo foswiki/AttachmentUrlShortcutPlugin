bd33d081fe8a8476844643559c47147ae861b546
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/AttachmentUrlShortcutPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/AttachmentUrlShortcutPlugin.pm
+++ lib/Foswiki/Plugins/AttachmentUrlShortcutPlugin.pm
@@ -1,4 +1,4 @@
-# This script Copyright (c) 2008 Impressive.media 
+# This script Copyright (c) 2008 Impressive.media
 # and distributed under the GPL (see below)
 #
 # This program is free software; you can redistribute it and/or
@@ -9,19 +9,20 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
-
 
 package Foswiki::Plugins::AttachmentUrlShortcutPlugin;
 
 use strict;
 use Assert;
-use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
+use vars
+  qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
 
 $VERSION = '$Rev: 12445$';
 $RELEASE = 'Dakar';
-$SHORTDESCRIPTION = 'Lets you use shortened syntax for links for attachments with %A%';
+$SHORTDESCRIPTION =
+  'Lets you use shortened syntax for links for attachments with %A%';
 
 $NO_PREFS_IN_TOPIC = 1;
 
@@ -29,31 +30,40 @@ $NO_PREFS_IN_TOPIC = 1;
 $pluginName = 'AttachmentUrlShortcutPlugin';
 
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $Foswiki::Plugins::VERSION < 1.026 ) {
-        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $Foswiki::Plugins::VERSION < 1.026 ) {
+        Foswiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
-    my $marcoName = $Foswiki::cfg{Plugins}{AttachmentUrlShortcutPlugin}{MacroName};
+    my $marcoName =
+      $Foswiki::cfg{Plugins}{AttachmentUrlShortcutPlugin}{MacroName};
     Foswiki::Func::registerTagHandler( $marcoName, \&_AURLSHORT );
 
     return 1;
 }
 
 sub _AURLSHORT {
-    my($this, $params, $topic, $web) = @_;
-    $web = $params->{'Bereich'} || $params->{'web'} || $web;
+    my ( $this, $params, $topic, $web ) = @_;
+    $web   = $params->{'Bereich'}  || $params->{'web'}   || $web;
     $topic = $params->{'Dokument'} || $params->{'topic'} || $topic;
-    my $file = $params->{'Anhang'} || $params->{'name'} || $params->{_DEFAULT} || "";
+    my $file =
+         $params->{'Anhang'}
+      || $params->{'name'}
+      || $params->{_DEFAULT}
+      || "";
     my $name = $params->{'Name'} || $params->{'label'} || $file;
     my $url;
-    if($file eq "") {
-       return Foswiki::Func::getScriptUrl($web, $topic, 'viewfile', 'filename'=>'');	
+    if ( $file eq "" ) {
+        return Foswiki::Func::getScriptUrl( $web, $topic, 'viewfile',
+            'filename' => '' );
     }
+
     #else
-    $url = Foswiki::Func::getScriptUrl($web, $topic, 'viewfile', 'filename'=>$file);
+    $url = Foswiki::Func::getScriptUrl( $web, $topic, 'viewfile',
+        'filename' => $file );
     return "[[$url][$name]]";
 }
 
